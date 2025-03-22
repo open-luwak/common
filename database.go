d1a5c4dd6a89b9e25d389e7e1badd210fad9201e
yitype <zhao5908@gmail.com>
2025-03-22 16:28:51 +0800

    Move Repository interface to persistence.go

--- database.go
+++ database.go
@@ -17,27 +17,3 @@ type PreparedQuery struct {
 	DatabaseUpdater []*PreparedQuery `json:"databaseUpdater"`
 	ResultEnricher  []*PreparedQuery `json:"resultEnricher"`
 }
-
-type Repository interface {
-	BindNamed(query string, arg any) (string, []any, error)
-	Find(query string, args ...any) (map[string]any, error)
-	FindAll(query string, args ...any) ([]map[string]any, error)
-	Insert(query string, args ...any) (int64, error)
-	InsertBatch(query string, args ...any) (int64, error)
-	// InsertNamedBatch
-	// Deprecated
-	InsertNamedBatch(query string, args []any) (int64, error)
-	Update(query string, args ...any) (int64, error)
-	Delete(query string, args ...any) (int64, error)
-
-	Begin() error
-	Commit() error
-	Rollback() error
-
-	Exec(query string, args ...any) error
-
-	DriverName() string
-
-	QueryTotal() int64
-	SetTraceId(traceId string)
-}
