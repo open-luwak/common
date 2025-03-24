51f2f4e4c87c170523576329a8c63e4795e93359
yitype <zhao5908@gmail.com>
2025-03-25 04:56:13 +0800

    Add API context and related interfaces

---
+++ api.go
@@ -0,0 +1,41 @@
+package common
+
+type APIContextKey struct{}
+
+// APIContext combines request and response contexts
+type APIContext interface {
+	RequestContext
+	ResponseContext
+	MutableRequestContext
+	MutableResponseContext
+}
+
+// RequestContext contains request-related information
+type RequestContext interface {
+	RequestID() string
+	Method() string
+	Params() any
+	RawParams() []byte
+	Metas() map[string]any
+}
+
+// ResponseContext contains response-related information
+type ResponseContext interface {
+	Result() any
+	Err() error
+}
+
+// MutableRequestContext allows modification of request context
+type MutableRequestContext interface {
+	SetRequestID(string)
+	SetMethod(string)
+	SetParams(any)
+	SetRawParams([]byte)
+	SetMetas(map[string]any)
+}
+
+// MutableResponseContext allows modification of response context
+type MutableResponseContext interface {
+	SetResult(any)
+	SetErr(error)
+}
