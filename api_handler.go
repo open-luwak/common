2539af5e4fb836e11b7a6e30ddbbe87bb7e3ed38
yitype <zhao5908@gmail.com>
2025-03-30 15:30:17 +0800

    Remove unused Chain and related methods

--- api_handler.go
+++ api_handler.go
@@ -9,24 +9,3 @@ type HandlerFunc func(APIContext) error
 func (hf HandlerFunc) Process(ctx APIContext) error {
 	return hf(ctx)
 }
-
-type Chain struct {
-	list []func(Handler) Handler
-}
-
-func NewChain() *Chain {
-	return &Chain{
-		list: make([]func(Handler) Handler, 0, 8),
-	}
-}
-
-func (c *Chain) Append(middleware func(Handler) Handler) {
-	c.list = append(c.list, middleware)
-}
-
-func (c *Chain) Apply(h Handler) Handler {
-	for i := len(c.list) - 1; i >= 0; i-- {
-		h = c.list[i](h)
-	}
-	return h
-}
