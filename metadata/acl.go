b6e284f8a6ba682c962d93603110ec07ce52ffb3
yitype <zhao5908@gmail.com>
2025-07-04 11:14:05 +0800

    access is denied by default.

--- metadata/acl.go
+++ metadata/acl.go
@@ -7,7 +7,7 @@ type Acl struct {
 type Role struct {
 	Name          string  `toml:"name"`
 	Description   string  `toml:"description"`
-	DefaultAccess string  `toml:"default_access"`
+	DefaultAccess string  `toml:"default_access,omitempty"`
 	Allow         []Allow `toml:"allow,omitempty"`
 	Deny          []Deny  `toml:"deny,omitempty"`
 }
