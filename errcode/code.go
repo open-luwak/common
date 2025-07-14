a4d4f66d87766b26cd585a435b1aa4c23ef61a79
yitype <zhao5908@gmail.com>
2025-07-14 11:40:29 +0800

    errcode

--- errcode/code.go
+++ errcode/code.go
@@ -1,7 +1,46 @@
 package errcode
 
+// gateway
+const (
+	EndpointEnvExpress    = "SP.Endpoint.EnvExpression"
+	EndpointFilterExpress = "SP.Endpoint.FilterExpression"
+	EndpointValueExpress  = "SP.Endpoint.ValueExpression"
+
+	APINotFound         = "DEV.API.NotFound"
+	APIStateDraft       = "DEV.API.State.Draft"
+	APIStateOffline     = "DEV.API.State.Offline"
+	APIStateRemoved     = "DEV.API.State.Removed"
+	APIRequestIdUsed    = "AD.API.RequestIdUsed"
+	APIInvalidTimestamp = "AD.API.InvalidTimestamp"
+	APIInvalidSignature = "AD.API.InvalidSignature"
+	APIInvalidAppKey    = "AD.API.InvalidAppKey"
+	APIUnauthorized     = "EU.API.Unauthorized"
+)
+
+// firewall
 const (
 	FwMissingMetasParameter = "EU.CheckSign.MissingMetasParameter"
 	FwInvalidAppKey         = "EU.CheckSign.InvalidAppKey"
 	FwInvalidSignature      = "EU.CheckSign.InvalidSignature"
+)
+
+// core
+const (
+	ScriptParse     = "BE.Script.Parse"
+	ScriptExecution = "BE.Script.Execution"
+	ScriptTimeout   = "BE.Script.Timeout"
+)
+
+// infrastructure
+const (
+	DbConnect    = "OPS.DbConnect"
+	RedisConnect = "OPS.RedisConnect"
+	MqConnect    = "OPS.MqConnect"
+)
+
+// license
+const (
+	LicenseExpired    = "OPS.LicenseExpired"
+	LicenseInvalid    = "OPS.LicenseInvalid"
+	LicenseRestricted = "OPS.LicenseRestricted"
 )
