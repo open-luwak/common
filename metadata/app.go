51d0994bcb51096c9267b4be61ec8948702147d7
yitype <zhao5908@gmail.com>
2025-07-17 09:06:57 +0800

    app instance

--- metadata/app.go
+++ metadata/app.go
@@ -1,8 +1,8 @@
 package metadata
 
 type AppInstance struct {
-	Roles   []string       `toml:"roles,omitempty"`
-	Key     string         `toml:"key"`
-	Secret  string         `toml:"secret"`
-	Session map[string]any `toml:"session,omitempty"`
+	Software string         `toml:"software"`
+	Key      string         `toml:"key"`
+	Secret   string         `toml:"secret"`
+	Session  map[string]any `toml:"session,omitempty"`
 }
