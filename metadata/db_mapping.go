d192446763e76ad470d97a542e3ff6a689fbabd8
yitype <zhao5908@gmail.com>
2025-07-28 03:37:20 +0800

    db mapping version omitempty

--- metadata/db_mapping.go
+++ metadata/db_mapping.go
@@ -8,6 +8,6 @@ type DBMapping struct {
 	LogicalName string `toml:"logical_name,omitempty"`
 	RealName    string `toml:"real_name,omitempty"`
 
-	Type    string `toml:"type"`
-	Version string `toml:"version"`
+	Type    string `toml:"type,omitempty"`
+	Version string `toml:"version,omitempty"`
 }
