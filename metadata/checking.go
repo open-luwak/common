685b872f50514bd04c07ff3f57076fb863155ae5
yitype <zhao5908@gmail.com>
2025-08-25 16:22:25 +0800

    rename checking

--- metadata/checking.go
+++ metadata/checking.go
@@ -1,6 +1,6 @@
 package metadata
 
 type CheckItem struct {
-	Expression string `toml:"expression"`
-	Message    string `toml:"message,omitempty"`
+	Check   string `toml:"check"`
+	Message string `toml:"message,omitempty"`
 }
