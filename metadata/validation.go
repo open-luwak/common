8ba43afce5b724f3ce48c5d9599b9196a201df33
yitype <zhao5908@gmail.com>
2025-08-27 14:03:45 +0800

    validation add label

--- metadata/validation.go
+++ metadata/validation.go
@@ -3,6 +3,7 @@ package metadata
 type ValidationRule struct {
 	Field string      `toml:"field"`
 	Type  string      `toml:"type,omitempty"`
+	Label string      `toml:"label,omitempty"`
 	Rules []*RuleItem `toml:"rules,omitempty"`
 }
 
