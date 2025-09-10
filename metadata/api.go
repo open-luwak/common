1e0d9615783ec89d9cb2467e79efee74f668ce15
yitype <zhao5908@gmail.com>
2025-09-10 10:31:21 +0800

    add api context

--- metadata/api.go
+++ metadata/api.go
@@ -11,6 +11,7 @@ type Api struct {
 	ExecuteGlobalScript bool   `toml:"execute_global_script"`
 	Enabled             bool   `toml:"enabled"`
 
+	Context      map[string]any    `toml:"context,omitempty"`
 	AutoFilter   []*AutoFilter     `toml:"auto_filter,omitempty"`
 	AutoPopulate []*AutoPopulate   `toml:"auto_populate,omitempty"`
 	Validation   []*ValidationRule `toml:"validation,omitempty"`
