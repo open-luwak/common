d42cd3b2af3353c4968e14d937dc06029e6c16a5
yitype <zhao5908@gmail.com>
2025-10-19 18:55:16 +0800

    script globalThis

--- script/global.go
+++ script/global.go
@@ -45,3 +45,51 @@ func (m GlobalThis) Metas() map[string]any {
 func (m GlobalThis) SetMetas(metas map[string]any) {
 	m[metaKey] = metas
 }
+
+func (m GlobalThis) Server() map[string]any {
+	if v, ok := m[serverKey]; ok {
+		if server, ok := v.(map[string]any); ok {
+			return server
+		}
+	}
+
+	server := make(map[string]any)
+	m[serverKey] = server
+	return server
+}
+
+func (m GlobalThis) SetServer(server map[string]any) {
+	m[serverKey] = server
+}
+
+func (m GlobalThis) Session() map[string]any {
+	if v, ok := m[sessionKey]; ok {
+		if sess, ok := v.(map[string]any); ok {
+			return sess
+		}
+	}
+
+	sess := make(map[string]any)
+	m[sessionKey] = sess
+	return sess
+}
+
+func (m GlobalThis) SetSession(sess map[string]any) {
+	m[sessionKey] = sess
+}
+
+func (m GlobalThis) Env() map[string]any {
+	if v, ok := m[envKey]; ok {
+		if env, ok := v.(map[string]any); ok {
+			return env
+		}
+	}
+
+	env := make(map[string]any)
+	m[envKey] = env
+	return env
+}
+
+func (m GlobalThis) SetEnv(env map[string]any) {
+	m[envKey] = env
+}
