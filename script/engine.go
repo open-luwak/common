b6e168c4ccd5890283f37baecbc2c883e10e496f
yitype <zhao5908@gmail.com>
2025-11-24 17:27:56 +0800

    script.RunnerContext add MetaProvider

--- script/engine.go
+++ script/engine.go
@@ -1,17 +1,20 @@
 package script
 
 import (
+	"github.com/open-luwak/common/api"
 	"github.com/open-luwak/common/dbop"
 )
 
 type RunnerContext struct {
 	// Provide the default entity name for getting database connections
 	// only when the scripting language is native SQL.
+	//
+	// Deprecated
 	DefaultEntityName string
 
 	GlobalThis map[string]any
+	DebugInfo  map[string]any
 
-	DalCtx *dbop.DalContext
-
-	DebugInfo map[string]any
+	DalCtx       *dbop.DalContext
+	MetaProvider *api.MetaProvider
 }
