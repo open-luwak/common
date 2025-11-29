a55e86194b988cf3f4e97df9018d720fc14348a8
yitype <zhao5908@gmail.com>
2025-11-29 16:58:42 +0800

    json tag

--- metadata/validation.go
+++ metadata/validation.go
@@ -1,17 +1,17 @@
 package metadata
 
 type ValidationRule struct {
-	Field string      `toml:"field"`
-	Type  string      `toml:"type,omitempty"`
-	Label string      `toml:"label,omitempty"`
-	Rules []*RuleItem `toml:"rules,omitempty"`
+	Field string      `toml:"field" json:"field"`
+	Type  string      `toml:"type,omitempty" json:"type,omitempty"`
+	Label string      `toml:"label,omitempty" json:"label,omitempty"`
+	Rules []*RuleItem `toml:"rules,omitempty" json:"rules,omitempty"`
 }
 
 type RuleItem struct {
-	Name      string `toml:"name,omitempty"`
-	Message   string `toml:"message,omitempty"`
-	Validator string `toml:"validator"`
-	Value     any    `toml:"value,omitempty"`
+	Name      string `toml:"name,omitempty" json:"name,omitempty"`
+	Message   string `toml:"message,omitempty" json:"message,omitempty"`
+	Validator string `toml:"validator" json:"validator"`
+	Value     any    `toml:"value,omitempty" json:"value,omitempty"`
 }
 
 type ValidationResult struct {
