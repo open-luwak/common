01b26ff493af378eb62307730b2716db59dd6ff8
yitype <zhao5908@gmail.com>
2025-12-03 11:35:57 +0800

    error code

--- errcode/code.go
+++ errcode/code.go
@@ -1,44 +1,8 @@
 package errcode
 
-// end user = EU
-// app developer = AD
-// biz error = BIZ
-// operations = OPS
-
 const (
-	APIScheduledTaskNotAllowed = "AD.API.ScheduledTaskNotAllowed"
-	APIInvalidMethodName       = "AD.API.InvalidMethodName"
-	APINotFound                = "AD.API.NotFound"
-	APIStateDraft              = "AD.API.StateDraft"
-	APIStateOffline            = "AD.API.StateOffline"
-	APIStateRemoved            = "AD.API.StateRemoved"
-	APIRequestIdUsed           = "AD.API.RequestIdUsed"
-	APIMissingMetasParameter   = "AD.API.MissingMetasParameter"
-	APIInvalidTimestamp        = "AD.API.InvalidTimestamp"
-	APIInvalidSignature        = "AD.API.InvalidSignature"
-	APIUnauthorized            = "EU.API.Unauthorized"
-
-	APPInvalidAppKey     = "AD.APP.InvalidAppKey"
-	APPEmptyAppKey       = "AD.APP.EmptyAppKey"
-	APPEmptySoftwareName = "AD.APP.EmptySoftwareName"
-
-	SoftwareInvalidSoftwareName = "AD.Software.InvalidSoftwareName"
-	SoftwarePrivilegeNotGranted = "AD.Software.PrivilegeNotGranted"
-
-	AclRoleNotAllowed = "AD.ACL.RoleNotAllowed"
-
-	ScriptParseFailed          = "AD.Script.ParseFailed"
-	ScriptExecutionFailed      = "AD.Script.ExecutionFailed"
-	ScriptExecutionTimeout     = "AD.Script.ExecutionTimeout"
-	ScriptLangNotSupported     = "AD.Script.LangNotSupported"
-	ScriptExecutorNotAvailable = "AD.Script.ExecutorNotAvailable"
-
-	DatabaseQueryFailed = "AD.Database.QueryFailed"
-
-	DbConnectFailed    = "OPS.Database.ConnectFailed"
-	RedisConnectFailed = "OPS.Redis.ConnectFailed"
-	MqConnectFailed    = "OPS.Mq.ConnectFailed"
-	LicenseExpired     = "OPS.License.Expired"
-	LicenseInvalid     = "OPS.License.Invalid"
-	LicenseRestricted  = "OPS.License.Restricted"
+	APIUnauthorized             = "EU.API.Unauthorized"                // 401 Unauthorized
+	SoftwarePrivilegeNotGranted = "AD.Software.Permission.NotAssigned" // 403 Forbidden
+	APIPermissionDeniedAccess   = "EU.API.Permission.Denied"           // 403
+	APIRateLimitExceeded        = "SP.API.RateLimit.Exceeded"          // 429 Too Many Requests
 )
