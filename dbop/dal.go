3fa1062a811c9a0a1a70d40ed29a4d4bd06aad9b
yitype <zhao5908@gmail.com>
2025-12-07 17:46:43 +0800

    change query result struct

--- dbop/dal.go
+++ dbop/dal.go
@@ -17,16 +17,35 @@ const (
 	Transaction
 )
 
+func (t ResultType) String() string {
+	switch t {
+	case None:
+		return "none"
+	case MultiRows:
+		return "multi_rows"
+	case SingleRow:
+		return "single_row"
+	case AutoSingleColumn:
+		return "auto_single_column"
+	case SingleValue:
+		return "single_value"
+	case LastInsertId:
+		return "last_insert_id"
+	case RowsAffected:
+		return "rows_affected"
+	case Transaction:
+		return "transaction"
+	default:
+		return "unknown"
+	}
+}
+
 type QueryResult struct {
-	ResultName string
-	ResultType ResultType
-
-	Rows         []map[string]any
-	Row          map[string]any
-	Value        any
-	LastInsertId int64
-	RowsAffected int64
-
+	ResultType     ResultType
+	LastInsertId   int64
+	RowsAffected   int64
+	Row            map[string]any
+	Rows           []map[string]any
 	CheckPoint     []*QueryResult
 	ResultEnricher []*QueryResult
 }
@@ -47,6 +66,37 @@ const (
 	TransactionAction
 	DDLAction
 )
+
+func (t OperationType) String() string {
+	switch t {
+	case InvalidAction:
+		return "invalid_action"
+	case CreateOneAction:
+		return "create_one_action"
+	case CreateManyAction:
+		return "create_many_action"
+	case ReadManyAction:
+		return "read_many_action"
+	case ReadOneAction:
+		return "read_one_action"
+	case ReadValueAction:
+		return "read_value_action"
+	case UpdateOneAction:
+		return "update_one_action"
+	case UpdateManyAction:
+		return "update_many_action"
+	case DeleteOneAction:
+		return "delete_one_action"
+	case DeleteManyAction:
+		return "delete_many_action"
+	case TransactionAction:
+		return "transaction_action"
+	case DDLAction:
+		return "ddl_action"
+	default:
+		return "unknown"
+	}
+}
 
 type DalContext struct {
 	// Container for transaction management.
