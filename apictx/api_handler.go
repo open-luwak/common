976d603424348f7a68155aea579bad1549553c32
yitype <zhao5908@gmail.com>
2026-07-18 17:27:54 +0800

    refactor: api context

--- apictx/api_handler.go
+++ apictx/api_handler.go
@@ -1,12 +1,12 @@
 package apictx
 
 type Handler interface {
-	Handle(Context) error
+	Handle(*Context) error
 }
 
-type HandlerFunc func(Context) error
+type HandlerFunc func(*Context) error
 
-func (hf HandlerFunc) Handle(ctx Context) error {
+func (hf HandlerFunc) Handle(ctx *Context) error {
 	return hf(ctx)
 }
 
