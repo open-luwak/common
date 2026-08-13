30ab4f2e861e03e6d64a7cc5079d3299b301fb87
yitype <zhao5908@gmail.com>
2026-08-13 12:07:32 +0800

    change appctx

--- appctx/context.go
+++ appctx/context.go
@@ -3,18 +3,26 @@ package appctx
 import "context"
 
 type Context struct {
-	AppRoot   string
-	Namespace string
-	KeyPrefix string
+	appRoot   string
+	namespace string
+	keyPrefix string
 }
+
+func New(appRoot, namespace, keyPrefix string) *Context {
+	return &Context{appRoot, namespace, keyPrefix}
+}
+
+func (c *Context) AppRoot() string   { return c.appRoot }
+func (c *Context) Namespace() string { return c.namespace }
+func (c *Context) KeyPrefix() string { return c.keyPrefix }
 
 type contextKey struct{}
 
-func With(ctx context.Context, info Context) context.Context {
-	return context.WithValue(ctx, contextKey{}, info)
+func With(ctx context.Context, cfg *Context) context.Context {
+	return context.WithValue(ctx, contextKey{}, cfg)
 }
 
-func From(ctx context.Context) (Context, bool) {
-	info, ok := ctx.Value(contextKey{}).(Context)
-	return info, ok
+func From(ctx context.Context) (*Context, bool) {
+	cfg, ok := ctx.Value(contextKey{}).(*Context)
+	return cfg, ok
 }
